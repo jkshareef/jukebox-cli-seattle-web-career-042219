f2b7e8bcb998eccc9a9af72f12be3ede2666141e
Jamal Shareef <theepie@hotmail.com>
2019-04-10 00:57:16 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -44,13 +44,12 @@ def play(my_songs)
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
   
-  array = Array.new
-  my_songs.each do |key, value|
-    array.push(key)
-  end
-  
   puts "Please enter a song name"
   song_name = gets.chomp
+  
+  if my_songs.keys.include?(song_name)
+    
+    
     
   
 end
